b4486f219eb860daf1466ea875cb71a52346fc87
graham <gpaye8@gmail.com>
2017-11-14 19:28:04 -0800

    add store, root reducer, and atomics

---
+++ src/reducers/root_reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import GamesReducer from './games_reducer';
+// import SessionReducer from './session_reducer';
+
+const RootReducer = combineReducers({
+});
+
+export default RootReducer;
